b7604cb5bfec5ab32b7296afe77db4620506c177
R1CH1R0Y <richiroy.me@gmail.com>
2024-06-13 01:12:48 +0530

    schema finished

---
+++ models/task.jsx
@@ -0,0 +1,11 @@
+const mongoose=require("mongoose")
+const schema=mongoose.Schema(
+    {
+    "tname":{type:String,required:true},
+    "date":{type:String,required:true},
+    "priority":{type:String,required:true},
+    "tdesc":{type:String,required:true},
+    }
+)
+let taskmodel=mongoose.model("Tasks",schema);
+module.exports={taskmodel}
