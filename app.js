b9fded69b029ac727c8ef4331e7216f0848e1e6d
R1CH1R0Y <richiroy.me@gmail.com>
2024-06-13 01:20:59 +0530

    succefully tested and completed add api on terminal and database

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const {taskmodel}=require("./models/task")
+
+const app=express()
+app.use(cors())
+app.use(express.json())
+
+mongoose.connect("mongodb+srv://Richi2001:R1CH1R0Y@cluster0.ulfkc.mongodb.net/TaskDB?retryWrites=true&w=majority&appName=Cluster0")
+
+
+app.post("/add",(req,res)=>{
+    let input=req.body
+    let task=new taskmodel(input)
+    console.log(task)
+    task.save()
+    res.json({status:"success"})
+})
+
+app.listen(8235,()=>{
+    console.log("server started")
+})
