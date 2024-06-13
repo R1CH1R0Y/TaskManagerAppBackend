b4f9ddd4ae41ac36ab1ea8db58ac5d8f7f78bd35
R1CH1R0Y <richiroy.me@gmail.com>
2024-06-13 06:26:59 +0530

    backend finished

--- models/task.js
+++ models/task.js
@@ -4,7 +4,7 @@ const schema=mongoose.Schema(
     "tname":{type:String,required:true},
     "date":{type:String,required:true},
     "priority":{type:String,required:true},
-    "tdesc":{type:String,required:true},
+    "tdesc":{type:String,required:true}
     }
 )
 let taskmodel=mongoose.model("Tasks",schema);
